bf1d75704c306e459ddda64f7b492dd252323f4c
Hvmble <mmontoyaabadia@gmail.com>
2018-08-20 23:31:56 -0500

    update

---
+++ IF/B.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Montoya Abadia
+ * Date: 20/08/2018
+ * Time: 10:37 PM
+ */
+
+$numero = 17;
+
+for ($i = 1; $i <= $numero; $i ++){
+    if ($numero%$i == 0){
+        $num++;
+    }
+}
+if ($num == 2){
+    echo "Es un numero primo";
+}else{
+    echo "No es un numero primo";
+}
